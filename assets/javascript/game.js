637845afa7ea5b4f17a5a6c2e3ca7817066d4143
Charles Ladesic <cladesic.student@gmail.com>
2019-02-21 15:23:16 -0600

    added replay button

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,51 +1,43 @@
-$(document).ready(function() {
 
+$(document).ready(function () {
+    $("button").click(function(){
+        location.reload(true);
+
+    });
     var letters = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z", "_"];
     var wins = 0;
     var lose = 0;
-    var guessLeft=10;
-
-    // for (var i=0; i<10;i++){
-    document.onkeyup = function(event){
+    var guessLeft = 10;
     
-    var userGuess = event.key;
-     $("#lettersGuessed").append(userGuess);
 
-    var computerGuess = letters[Math.floor(Math.random()* letters.length)];
-    console.log(computerGuess);
-   
-  
-        if(userGuess === computerGuess){
+    document.onkeyup = function (event) {
+
+        var userGuess = event.key;
+        $("#lettersGuessed").append(userGuess);
+
+        var computerGuess = letters[Math.floor(Math.random() * letters.length)];
+        console.log(computerGuess);
+
+        if (userGuess === computerGuess) {
             wins++;
             $(".badge-success").text(wins)
             console.log("You win " + wins);
-        } else if((userGuess!== computerGuess) && (guessLeft !== 0)) {
+        } else if ((userGuess !== computerGuess) && (guessLeft !== 0)) {
             lose++;
             guessLeft--;
             $(".badge-error").text(lose);
             $(".badge-primary").text(guessLeft);
-        
-      console.log("You lose " + lose +"  " + "Guesses " + guessLeft);
+
+            console.log("You lose " + lose + "  " + "Guesses " + guessLeft);
         } else if (guessLeft === 0) {
-       
-        console.log("Game Over you lose" + guessLeft);
-        alert("You lose the game!");
+
+            console.log("Game Over you lose" + guessLeft);
+            alert("You lose the game!")
+           
+
         }
-       return $("#lettersGuessed", ".badge-success",".badge-error", ".badge-primary");
-        
-  
-
-
+        return $("#lettersGuessed", ".badge-success", ".badge-error", ".badge-primary");
 
     }
- // };
-
-
-
-
-
-
-
-
 
 });
